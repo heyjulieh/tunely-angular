80d9c29fa369bddc7efaabcaa3b0b846e66e789e
heyjulieh <heyjulieh@gmail.com>
2017-04-25 16:05:28 -0700

    delete works, update doesn't yet

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -27,15 +27,43 @@ function AlbumsIndexController ($http) {
     console.log('There was an error getting the data', response);
   });
 
-  vm.createAlbum = function () {
-    $http({
-      method: 'POST',
-      url: '/api/albums',
-      data: vm.newAlbum,
-    }).then(function successCallback(response) {
-      vm.albums.push(response.data);
-    }, function errorCallback(response) {
-      console.log('There was an error posting the data', response);
-    });
-  }
+    vm.createAlbum = function () {
+      $http({
+        method: 'POST',
+        url: '/api/albums',
+        data: vm.newAlbum,
+      }).then(function successCallback(response) {
+        vm.albums.push(response.data);
+      }, function errorCallback(response) {
+        console.log('There was an error posting the data', response);
+      });
+    }
+
+    vm.deleteAlbum = function (album) {
+      $http({
+        method: 'DELETE',
+        url: '/api/albums/'+ album._id
+      }).then(function successCallback(json) {
+        var index = vm.albums.indexOf(album);
+        console.log("index is: " + index);
+        vm.albums.splice(index,1)
+      }, function errorCallback(response) {
+        console.log('There was an error deleting the data', response);
+      });
+    }
+
+    vm.editAlbum = function (album) {
+      $http({
+        method: 'PUT',
+        url: '/api/albums/'+ album._id,
+        data: album
+      }).then(function successCallback(json) {
+        console.log(json);
+        var index = vm.albums.indexOf(album);
+        console.log("index is: " + index);
+        vm.albums.splice(index,1,json)
+      }, function errorCallback(response) {
+        console.log('There was an error updating the data', response);
+      });
+    }
 }
