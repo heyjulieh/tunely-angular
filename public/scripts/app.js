6f7dfc95da730cc0db0ccd83ba3b9646b58898b4
heyjulieh <heyjulieh@gmail.com>
2017-04-25 15:06:02 -0700

    sprint 2 complete with ability to add new album with genres and release date

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -11,27 +11,40 @@ angular
   // the second argument is a function that defines the capacities
   // of the controller.
 
-function AlbumsIndexController () {
+AlbumsIndexController.$inject = ['$http'];
+function AlbumsIndexController ( $http ) {
   var vm = this;
-  vm.newAlbum = {};
+  // vm.newAlbum = {};
+  //
+  // vm.newAlbum = {
+  //     name: 'Viva Hate',
+  //     artistName: 'Morrissey'
+  // };
 
-  vm.newAlbum = {
-      name: 'Viva Hate',
-      artistName: 'Morrissey'
-  };
+  // inside controller
 
-  vm.albums = [
-    {
-      name: 'Coming Home',
-      artistName: 'Leon Bridges'
-    },
-    {
-      name: 'Are We There',
-      artistName: 'Sharon Van Etten'
-    },
-    {
-      name: 'The Queen is Dead',
-      artistName: 'The Smiths'
-    }
-  ];
+  $http({
+    method: 'GET',
+    url: 'api/albums'
+  }).then(function successCallback(response) {
+    console.log('Success!', response);
+    vm.albums = response.data;
+  }, function errorCallback(response) {
+    console.log('There was an error getting the data', response);
+  });
+
+  vm.createAlbum = function () {
+    $http({
+      method: 'POST',
+      url: '/api/albums',
+      data: vm.newAlbum,
+    }).then(function successCallback(response) {
+      var genres = vm.albums.genres.join();
+        vm.albums.genres = genres;
+      console.log('Hi!', response);
+      vm.albums.push(response.data);
+    }, function errorCallback(response) {
+      console.log('There was an error posting the data', response);
+    });
+  }
 }
