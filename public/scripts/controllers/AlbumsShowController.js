4d9d742ab33cebc66ce19daed3ba89e4189c2335
heyjulieh <heyjulieh@gmail.com>
2017-04-27 11:54:30 -0700

    got new song to add but track number doesn't add

--- public/scripts/controllers/AlbumsShowController.js
+++ public/scripts/controllers/AlbumsShowController.js
@@ -17,4 +17,39 @@ function AlbumsShowController ($http, $routeParams) {
     console.log('There was an error getting the data', response);
   });
 
+  vm.createSong = function () {
+    $http({
+      method: 'POST',
+      url: '/api/albums',
+      data: vm.newSong,
+    }).then(function successCallback(response) {
+      vm.album.songs.push(response.data);
+    }, function errorCallback(response) {
+      console.log('There was an error posting the data', response);
+    });
+  }
+
+  vm.editAlbum = function (album) {
+    $http({
+      method: 'PUT',
+      url: '/api/albums/'+album._id,
+      data: album
+    }).then(function successCallback(json) {
+      // don't need to do anything!
+    }, function errorCallback(response) {
+      console.log('There was an error editing the data', response);
+    });
+  }
+
+  vm.deleteAlbum = function (album) {
+    $http({
+      method: 'DELETE',
+      url: '/api/albums/'+ album._id
+    }).then(function successCallback(json) {
+      var index = vm.albums.indexOf(album);
+      vm.albums.splice(index,1)
+    }, function errorCallback(response) {
+      console.log('There was an error deleting the data', response);
+    });
+  }
 }
